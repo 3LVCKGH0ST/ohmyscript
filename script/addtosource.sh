53f5772b3197f4ec119ec8db1351dcbe07daf623
Mohamed Balaguid <mbalagui@student.1337.ma>
2022-07-10 17:25:02 +0100

    restruct

---
+++ script/addtosource.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+SCRDIR="$PWD/script"
+
+cat ~/.zshrc | grep -q 'ohmyscript' && echo -n "" || echo "make -C $SCRDIR" >> ~/.zshrc 
+cat ~/.zshrc | grep -q 'chiblassa' && echo -n "" || echo -e "\nalias chiblassa=\"$SCRDIR/chiblassa.sh\"\n" >> ~/.zshrc;
+cat token | grep -q "XXXXX" && echo -n "Add your token please !" || cat token > mytoken
---
+++ script/getblackholedays.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+BLACKHOLESF=$(cat out | grep "emote-bh" | grep "title" | awk '{print $NF}' | tr "'" " " | awk '{print $2}' | tr '/' '-');
+BLACKHOLE=$(awk -F'-' '{printf("%04d-%02d-%02d\n",$3,$2,$1)}' <<< $BLACKHOLESF | tr '-' ',')
+
+echo "";
+echo -n -e $BLACKHOLESF ' \t\t\t\t\t\t ';python -c "from datetime import date; print (date($BLACKHOLE)-date($(date +%Y,%m,%d))).days, \" days left\";";
